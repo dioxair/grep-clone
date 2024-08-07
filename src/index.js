14e2126d94e8f3ba0c4b2ae66d0e620175db8e3d
Samuel Olagunju <me@samola.net>
2024-08-07 08:33:25 -0400

    add library for command handling lol

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-let args = [];
+const { Command } = require("commander");
+const program = new Command();
 
-for (i of process.argv) {
-  if (i.startsWith("--")) {
-    args.push(i);
-  }
+program
+  .name("grep-clone")
+  .description("A (bad) clone of grep in JavaScript")
+  .version("0.0.0");
+
+program
+  .requiredOption(
+    "-p",
+    "--path <string>",
+    "Pass a file paht as an argument to parse with regular expressions.",
+  )
+  .option("-r", "--regexp <string>", "Parse file using regular expressions");
+
+program.parse();
+
+const options = program.opts();
+
+if (options.p) {
+  console.log("path");
+}
+
+if (options.r) {
+  console.log("regexp");
 }
